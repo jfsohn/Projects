638815ba70a119a6abca0ca694e93837bf414339
Jake Fenton Sohn <jfsohn@silo.soic.indiana.edu>
2014-09-04 18:46:25 -0400

    auto-commit

--- labs/lab2/src/main/java/edu/indiana/c212/labs/two/Lab2.java
+++ labs/lab2/src/main/java/edu/indiana/c212/labs/two/Lab2.java
@@ -2,25 +2,48 @@ package edu.indiana.c212.labs.two;
 
 public class Lab2 {
 	public static int recursiveAdd(int x, int y){
-		//TODO 
-		//code to add numbers recursively
-		return -1;
-	}
+		if (y == 0) {
+			return x;
+		} else if (x == 0) {
+			return y;
+		} else {
+			return recursiveAdd( x + 1 , y - 1);
+		}
+}
+
 	public static int iterativeAdd(int x, int y){
-		//TODO  
-		//code to add numbers iteratively
-		return -1;
+		int i = 0;
+		while (i < y) {
+			x = x + 1;
+			i = i + 1;
+		}
+		return x;
 	}
 	public static int iterativeMult(int x, int y){
-		//TODO 
-		//code to multiply numbers iteratively 
-		return -1;
+	    if (y == 0) {
+	    	return 0;
+	    }   else {
+	    	  int i = x;
+	    	  while (y > 1) {
+	    		  x = iterativeAdd(x , i);
+	    	      y = y -1;
+	    	  }
+	    	  return x;
+	    }	
 	}
+	
 	public static int iterativeExp(int x, int exp){	
-		//TODO
-		//code to find exponential of a number iteratively
-		return -1;
+		int n = x;
+		if(exp == 0){
+			return 1;
+		}
+		else for(int i = 1; i < exp; i++) {
+			x = iterativeMult(x,n);
+		}
+		return x;
 	}
+		
+		
 
     public  static void printExpected(Object expected, Object actual){
         System.out.printf("Expected value is : %s, received value was: %s. \n", expected, actual);
