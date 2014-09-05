954fea5bf3613f8f58e06cea4a01ab156f1ab298
Jacob Read <jrread@indiana.edu>
2014-09-05 18:02:26 -0400

    Adding new HW1Files.

---
+++ assignments/hw1/src/main/java/edu/indiana/cs/c212/hw/one/BlockGenerator.java
@@ -0,0 +1,20 @@
+package edu.indiana.cs.c212.hw.one;
+
+public class BlockGenerator {
+
+	/* Method printTicTacToeBoard() that prints a tic-tac-toe board */
+	public static void printTicTacToeBoard() {
+
+	}
+
+	/* Method printStaircase() that prints a staircase */
+	public static void printStaircase() {
+
+	}
+	
+	public static void main(String[] args)
+	{
+		printTicTacToeBoard();
+		printStaircase();
+	}
+}
