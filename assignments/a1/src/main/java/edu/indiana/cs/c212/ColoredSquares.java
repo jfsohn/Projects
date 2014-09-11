9b219e5475189db45d1452e2fb85a5546950a719
Jake Fenton Sohn <jfsohn@silo.soic.indiana.edu>
2014-09-11 01:10:31 -0400

    auto-commit

--- assignments/a1/src/main/java/edu/indiana/cs/c212/ColoredSquares.java
+++ assignments/a1/src/main/java/edu/indiana/cs/c212/ColoredSquares.java
@@ -14,95 +14,115 @@ import javax.swing.JPanel;
  * For more details please make sure to check out the wiki. It has detailed instructions
  * about how to complete the assignment.
  */
+
 public class ColoredSquares extends JPanel {
+	static JPanel controlPanel;
 
-    /**
-     * *************************************************************************
-     * Class instance variables
-     * **************************************************************************
-     */
-
-    private Random randomNumberGenerator = new Random();
+	private Random randomNumberGenerator = new Random();
 
 
-    /**
-     * Constructor to set the size of window
-     */
-    public ColoredSquares() {
-        Dimension dimension = new Dimension(300, 300);
-        this.setPreferredSize(dimension);
-    }
+	/**
+	 * Constructor to set the size of window
+	 */
+	public ColoredSquares() {
+		Dimension dimension = new Dimension(300, 300);
+		this.setPreferredSize(dimension);
+	}
 
-    /****************************************************************************
-     * Instance Methods
-     ****************************************************************************/
+	/****************************************************************************
+	 * Instance Methods
+	 ****************************************************************************/
 
-    /**
-     * painting method on the JPanel
-     */
-    @Override
-    public void paintComponent(Graphics g) {
-        this.setBackground(BACKGROUND_COLOR);
+	/**
+	 * painting method on the JPanel
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		this.setBackground(BACKGROUND_COLOR);
 
-        // find number of rows and columns by 300/30
-
-
-        Graphics2D tablet2D = (Graphics2D) g;
-        // call drawBlock method to draw squares of required size on JPanel
-
+		
+		
+		// find number of rows and columns by 300/30
 		/* Add just two missing lines (two `for-loops', one inside the other) to this paint() method. (Number the
 		 * rows and columns counting from zero.) And to do that, all you really need to know is that you can access 
 		 * the variables 'numberOfRows' and 'numberOfColumns' from inside the paint() method. 
-		*/
+		 */
+		int row;
+		row = 0;
+		int numberofRows=10;
+		int numberofCols=10;
+		Graphics2D tablet2D = (Graphics2D) g;
+		while (row<10) {
+			int col=0;
+			while (col<10){
+				drawBlock(tablet2D,row,col);
+				col++;  }
+			row++;}
 
-        //	drawBlock(tablet2D, row, column);
-    }
 
-    private void drawBlock(Graphics2D tablet, int row, int column) {
-        int xOrigin, yOrigin, width, height;
-        Rectangle rectangle;
-    }
 
-    private Color makeRandomColor() {
-        // This method will create random color and return it to drawBlock() method.
-        // Use makeRandomColorValue() method to generate random color value
-        // return new Color(<redColorValue>, <greenColorValue>, <blueColorValue>);
-        return null;
-    }
 
-    // this method generates random integer number between 0 (inclusive) and the specified value (exclusive)
-    private int randomColorValue() {
-        return randomNumberGenerator.nextInt(COLOR_RANGE);
-    }
 
-    // The entry main() method
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Colored Squares");
+		// call drawBlock method to draw squares of required size on JPanel
 
-        ColoredSquares coloredSquares = new ColoredSquares();
 
-        frame.setContentPane(coloredSquares);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack(); // "this" JFrame will be resized to pack all of its components
-        frame.setResizable(true); // Sets whether this frame is resizable by the user.
-        frame.setVisible(true); // show it
-    }
 
-    /**
-     * *************************************************************************
-     * Class constants
-     * **************************************************************************
-     */
 
-    public static final int INNER_SQUARE_SIZE = 25; // pixels
-    public static final int OUTLINE_THICKNESS = 1; // pixels
-    public static final int BACKGROUND_THICKNESS = 3; // pixels
-    public static final int OUTER_SQUARE_SIZE = INNER_SQUARE_SIZE + OUTLINE_THICKNESS * 2;
-    public static final int BLOCK_SIZE = OUTER_SQUARE_SIZE+ BACKGROUND_THICKNESS;
 
-    public static final Color BACKGROUND_COLOR = Color.CYAN;
-    public static final Color OUTLINE_COLOR = Color.BLACK;
+		
+		//	drawBlock(tablet2D, row, column);
+	}
+	
+	// call drawBlock method to draw squares of required size on JPanel
+	private void drawBlock(Graphics2D tablet, int row, int column) {
+		int xOrigin, yOrigin, width, height;
+		Rectangle rectangle;
+		tablet.setColor(makeRandomColor());
+		tablet.fillRect(row*30,column*30,27,27);
+		 tablet.setColor(Color.BLACK);
+         tablet.drawRect(row*30,column*30,27,27);
 
-    public static final int COLOR_RANGE = 256;
+
+	}
+
+	private Color makeRandomColor() {
+		Color color1= new Color (randomColorValue(),randomColorValue(),randomColorValue());
+		return color1;
+	}
+
+	// this method generates random integer number between 0 (inclusive) and the specified value (exclusive)
+	private int randomColorValue() {
+		return randomNumberGenerator.nextInt(COLOR_RANGE);
+	}
+
+	// The entry main() method
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Colored Squares");
+
+		ColoredSquares coloredSquares = new ColoredSquares();
+
+		frame.setContentPane(coloredSquares);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack(); // "this" JFrame will be resized to pack all of its components
+		frame.setResizable(true); // Sets whether this frame is resizable by the user.
+		frame.setVisible(true); // show it
+	}
+
+	/**
+	 * *************************************************************************
+	 * Class constants
+	 * **************************************************************************
+	 */
+
+	public static final int INNER_SQUARE_SIZE = 25; // pixels
+	public static final int OUTLINE_THICKNESS = 1; // pixels
+	public static final int BACKGROUND_THICKNESS = 3; // pixels
+	public static final int OUTER_SQUARE_SIZE = INNER_SQUARE_SIZE + OUTLINE_THICKNESS * 2;
+	public static final int BLOCK_SIZE = OUTER_SQUARE_SIZE+ BACKGROUND_THICKNESS;
+
+	public static final Color BACKGROUND_COLOR = Color.CYAN;
+	public static final Color OUTLINE_COLOR = Color.BLACK;
+
+	public static final int COLOR_RANGE = 256;
 
 }
--- assignments/a1/src/main/java/edu/indiana/cs/c212/NumberProcessor.java
+++ assignments/a1/src/main/java/edu/indiana/cs/c212/NumberProcessor.java
@@ -10,25 +10,51 @@ public class NumberProcessor {
 	
 	//this method should return the sum all of the numbers between 1 and n
 	public static int sumSequence(int i) {
-		return -1;
+		int sum = 0;
+		while (i>0){
+			sum= sum++;
+			i= i++;
+		}
+		return sum;
 	}
+	
 
 	//this method should return the sum all of the EVEN numbers between 0 and n
 	//example: n=8    :   0 + 2 + 4 + 6 + 8 = 20
 	public static int sumEvenSequence(int n) {
-		 return -1;
+		 int answer = 0;
+		 int i = 1;
+		 while(i <= n) {
+			 answer = i + answer;
+			 i = i + 2;
+		 }
+		 return answer;
 	}
+			 
+		 
 
 	//this method should return the sum all of the ODD numbers between 1 and n
 	//example: n=7    :   1 + 3 + 5 + 7 = 16
 	public static int sumOddSequence(int n) {
-		 return -1;
-	}
+		int answer = 0;
+		for(int i = 1; i <= n; i = i + 2){
+			answer = answer + i;
+		}
+		return answer;
+		}
 
 	//this method should return the sum all of the PERFECT SQUARES between 1 and n
 	//example: n=26    :   1 + 4 + 9 + 16 + 25 = 55
 	public static int sumSquaresSequence(int n) {
-		 return -1;
+		int sum = 0;
+		int nextN = 0;
+		int add = 1;
+		while (nextN <= sum) {
+		    sum = sum + nextN;
+		    nextN = sum + add;
+		    add = add + 2;
+		}
+		 return sum;
 	}
 
 	public static void main(String[] args)
