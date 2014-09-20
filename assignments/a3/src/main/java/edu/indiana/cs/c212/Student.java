2fd1f218bb481ef1210ebb44f19897b6ef868f31
Jacob Read <jrread@indiana.edu>
2014-09-20 17:12:12 -0400

    Jon's comments without solutions.

--- assignments/a3/src/main/java/edu/indiana/cs/c212/Student.java
+++ assignments/a3/src/main/java/edu/indiana/cs/c212/Student.java
@@ -3,23 +3,28 @@ package edu.indiana.cs.c212;
 import java.util.UUID;
 
 /**
- * This is a simple class that models a student. It has the fields that you might
- * expect from a student.
+ * This is a simple class that models a student. Each student has a name,
+ * and a UUID (unique user ID).  For more information on UUIDs see:
+ * http://docs.oracle.com/javase/7/docs/api/java/util/UUID.html
  */
 public class Student{
+	//FIXME add instance variables here
 
+	
     /**
-     * This constructor should create a student with a
+     * This constructor should create a student without a name (name should be null)
      */
     public Student(){
+    	//FIXME
 
     }
 
     /**
-     * A constructor that creates a student
+     * A constructor that creates a student with the given name
      * @param name the name of the student.
      */
     public Student(String name){
+    	//FIXME
 
     }
 
@@ -34,14 +39,19 @@ public class Student{
     }
 
     /**
-     * Changes to student's name value to the specified value.
+     * Changes the student's name to given specified name.
      * @param name The new name for the student.
      */
     public void setName(String name){
         //FIXME
     }
-
+    
+    /**
+     * 
+     * @return the student's ID
+     */
     public UUID getId(){
+    	//FIXME
         return  null;
     }
 
@@ -50,7 +60,20 @@ public class Student{
      * @param id the new id value
      */
     public void setId(UUID id){
-        //FIXME Change the idValue
+        //FIXME Change the student's ID to id
     }
-
+    
+   
+    /**
+     * Checks to see if this student is the same as another student. 
+     * Returns true if this student is the same as the other student and false otherwise.
+     * 
+     * Note: you do not need to write or modify any part of this method.
+     * 
+     * @param other
+     * @return
+     */
+    public boolean equals(Student other){
+    	return ((this.getId().equals(other.getId())) && (this.getName().equals(other.getName())));
+    }
 }
