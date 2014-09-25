8e9360dabd2c0257ecaabbcf9659d527cd4439fe
ssnimbal <ssnimbal@github.iu.edu>
2014-09-24 20:17:48 -0400

    Adding lab5

---
+++ labs/lab5/src/edu/indiana/cs/c212/Employee.java
@@ -0,0 +1,77 @@
+package edu.indiana.cs.c212;
+
+/**
+ * This is a simple class that models an employee. Each employee has a name,
+ * salary and year of joining the company.
+ */
+
+public class Employee 
+{
+	
+	// FIXME add instance variables here
+	
+    /**
+     * This constructor should create an Employee with a name, salary, and the year they joined the company. 
+     */
+	public Employee(String empName, double salary, int joinYear)
+	{
+		// FIXME 
+	}
+
+    /**
+     * Returns the name of the employee. 
+     * @return The name of the employee
+     */
+	public String getEmpName() 
+	{
+		//FIXME 
+		return null;
+	}
+
+	/**
+     * Changes the employee's name to the specified name.
+     * @param name The new name for the employee
+     */
+	public void setEmpName(String empName) 
+	{
+		//FIXME
+	}
+
+	/**
+     * Returns the salary of the employee. 
+     * @return The salary of the employee
+     */
+	public Double getSalary() 
+	{
+		//FIXME
+		return null;
+	}
+
+	/**
+     * Changes the employee's salary to given salary.
+     * @param salary The new salary for the employee
+     */
+	public void setSalary(double salary) 
+	{
+		//FIXME
+	}
+
+   /**
+     * Returns the join year of the employee. 
+     * @return The join year of the employee
+     */
+	public Integer getJoinYear() 
+	{
+		//FIXME
+		return null;
+	}
+
+	/**
+     * Changes the employee's year of joining to given specified year.
+     * @param name The new year of joining for the employee
+     */
+	public void setJoinYear(int joinYear) 
+	{
+		//FIXME
+	}
+}
---
+++ labs/lab5/src/edu/indiana/cs/c212/Lab5.java
@@ -0,0 +1,117 @@
+package edu.indiana.cs.c212;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Lab5 
+{
+	List<Employee> empList;
+	
+    /**
+     * This constructor should create an instance of ArrayList to store list of employees. 
+     */
+	public Lab5()
+	{
+		//FIXME
+	}
+	
+	/**
+	 * @param Employee emp
+	 * 
+	 * addEmployee is a method that takes an Employee object and adds it to
+	 * ArrayList<Employee> that contains information of all employees
+	 * 
+	 * Operations to know for ArrayLists:
+	 *   Let list be an ArrayList<Employee> and emp be an Employee:
+	 *   	list.add(emp) will add emp to the end of your list
+	 *      
+	 * For more information on ArrayLists see:
+	 * http://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
+	 */
+	public static void addEmployee(Employee emp)
+	{
+		//FIXME
+	}
+	
+	/**
+	 * @param String
+	 * @return Employee
+	 * 
+	 * findEmployee is a method that takes an employee name as a parameter and
+	 * searches ArrayList<Employee> to find the employee with that name
+	 *
+	 * For more information on Iterators see:
+	 * http://docs.oracle.com/javase/7/docs/api/java/util/Iterator.html
+	 */
+	public static Employee findEmployee(String empName)
+	{
+		//FIXME
+		return null;
+	}
+	
+	/**
+	 * @param String empName
+	 * @return Employee
+	 * 
+	 * removeEmployee is a method that takes an employee name as a parameter and
+	 * searches ArrayList<Employee>, removing the employee with that name if they are
+	 * in the ArrayList
+	 */
+	public static void removeEmployee(String empName)
+	{
+		//FIXME
+	}
+	
+	/**
+	 * @return Employee
+	 * 
+	 * getMaxSalaryEmp is a method that searches ArrayList<Employee> 
+	 * and returns an employee with the maximum salary
+	 */
+	public static Employee getMaxSalaryEmp()
+	{
+		//FIXME
+		return null;
+	}
+	
+	/**
+	 * @param double percentage
+	 * @return Employee
+	 * 
+	 * raiseSalary is a method that raises an employee's salary 
+	 * by given percentage
+	 */
+	public static void raiseSalary(double percentage)
+	{
+		//FIXME
+	}
+	
+	/**
+	 * @param Employee emp
+	 * 
+	 * displayEmployee is a method that displays employee name, salary and join year to the console
+	 */
+	public static void displayEmployee(Employee emp)
+	{
+		//FIXME
+	}
+	
+	public static void main(String[] args)
+	{
+
+		// Add employees to the list
+		
+		// Find employee
+		
+		// display employee details
+		
+		// display maximum salary employee details
+		
+		// increase salary of all employees by 10 percent
+		
+		// delete employee from list
+		
+		// display employee details for all employees
+	}
+} 
