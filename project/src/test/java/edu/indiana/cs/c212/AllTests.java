6debf6c8b5bcae04e49e53d40f0fdeb0a6133bc5
Jacob Read <jrread@indiana.edu>
2014-11-06 15:14:13 -0500

    Adding milestone 2 tests + required framework.

--- project/src/test/java/edu/indiana/cs/c212/AllTests.java
+++ project/src/test/java/edu/indiana/cs/c212/AllTests.java
@@ -4,15 +4,20 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import edu.indiana.cs.c212.board.SimpleGameBoardTest;
+import edu.indiana.cs.c212.board.TileTest;
 import edu.indiana.cs.c212.gameMechanics.LoseByConnectingRulesTest;
 import edu.indiana.cs.c212.gameMechanics.MoveTest;
 import edu.indiana.cs.c212.gameMechanics.StandardRulesTest;
 
 
 @RunWith(Suite.class)
-@SuiteClasses({	MoveTest.class, 
+@SuiteClasses({ SimpleGameBoardTest.class, 
+				MoveTest.class, 
 				StandardRulesTest.class,
 				LoseByConnectingRulesTest.class,
+				TileTest.class,
+				SimpleGameBoardTest.class
 				})
 public class AllTests {
 
