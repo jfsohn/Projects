4bdd479280677fda85ba88ad614a7a1071185c9f
Jake Fenton Sohn <jfsohn@silo.soic.indiana.edu>
2014-11-18 19:46:43 -0500

    auto-commit

--- project/src/main/java/BoardSetupPanel.java
+++ project/src/main/java/BoardSetupPanel.java
@@ -1,4 +1,9 @@
+import java.awt.Panel;
+import java.awt.BorderLayout;
 
 public class BoardSetupPanel extends javax.swing.JPanel {
- 
+ JPanel size = new JPanel;
+ JBoard min = MIN_SUPPORTED_BOARD_SIZE 
+
 }
+
