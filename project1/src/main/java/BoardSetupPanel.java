28d0c1e2f0d5fd491f30919593045369f7fb5155
jfsohn <jfsohn@indiana.edu>
2019-11-02 15:12:18 -0400

    Remove duplicate directory

--- project1/src/main/java/BoardSetupPanel.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Panel;
-import java.awt.BorderLayout;
-
-public class BoardSetupPanel extends javax.swing.JPanel {
- JPanel size = new JPanel;
- JBoard min = MIN_SUPPORTED_BOARD_SIZE 
-
-}
-
